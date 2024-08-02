e8c8475721ae119c7ab61bcd3f4ccc469903740e
John <johannaraymendoza@gmail.com>
2024-08-02 11:54:21 +0800

    New updates
    Project Pages
    Contact Pages

--- src/components/mini_components/project_page.vue
+++ src/components/mini_components/project_page.vue
@@ -7,11 +7,11 @@
         </div>
         <div class="flex justify-center items-center w-full flex-wrap gap-2 sm:gap-5">
             <div v-for="items in experiences" :key="items"
-                class="border-2 border-gray-200 border-opacity-60 rounded-lg m-2 flex justify-center items-center md:flex-col w-full md:w-[40%] lg:w-[30%]">
-                <img class="w-full object-cover object-center rounded-t-lg" :src="items.image" alt="blog">
-                <div class=" p-5 lg:p-10 flex flex-col gap-3 text-center">
+                class="border-2 border-gray-200 border-opacity-60 rounded-lg h-full m-2 flex justify-center items-center md:flex-col w-full md:w-[40%] lg:w-[30%]">
+                <img class="w-full object-cover object-center rounded-t-lg size-48 sm:size-80" :src="items.image" alt="blog">
+                <div class=" p-5 lg:p-10 flex flex-col gap-3 text-center justify-center items-center w-full">
                     <h1 class="text-base sm:text-lg font-bold">{{ items.position }}</h1>
-                    <h1 class="text-base sm:text-lg font-medium">{{ items.description }}</h1>
+                    <h1 class="text-base font-medium hidden sm:block">{{ items.description }}</h1>
                     <button type="button"
                         class="text-sm sm:text-xl text-secondary border-2 bg-white border-secondary p-2 w-full rounded-xl border-radius shadow-normal"
                         @click="openPopup()">Read More</button>
