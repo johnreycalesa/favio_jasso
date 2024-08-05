1c28fa9eead7b260f0c570d58574ccf282bbbd5f
John <103416957+jeonlei-01@users.noreply.github.com>
2024-08-05 23:58:37 +0800

    New updates
    Change the z-index of navigation page.
    Change the styling of skills icons

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -15,10 +15,8 @@ const toggleNav = () => {
 </script>
 
 <template>
-  <div class="bg-tertiary w-full p-2 sm:p-5 fixed top-0 h-min shadow-md">
-    <header
-      class="container mx-auto flex w-full items-center relative justify-between py-4 px-2 sm:px-6"
-    >
+  <div class="bg-tertiary w-full p-2 sm:p-5 fixed top-0 h-min shadow-md z-50">
+    <header class="container mx-auto flex w-full items-center relative justify-between py-4 px-2 sm:px-6">
       <a href="#home">
         <div class="w-full text-center text-xl sm:text-4xl font-bold sm:w-fit sm:text-left">
           <span class="text-orange-950 font-bold">FAVIO JASSO</span>
@@ -27,120 +25,85 @@ const toggleNav = () => {
       <nav class="hidden bg-tertiary text-base md:block">
         <ul class="flex items-center space-x-2">
           <li class="group relative">
-            <a
-              class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
-              href="#home"
-            >
+            <a class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
+              href="#home">
               Home
             </a>
           </li>
           <li class="group relative">
-            <a
-              class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
-              href="#skills"
-            >
+            <a class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
+              href="#skills">
               Skills
             </a>
           </li>
           <li class="group relative">
-            <a
-              class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
-              href="#experience"
-            >
+            <a class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
+              href="#experience">
               Experience
             </a>
           </li>
           <li class="group relative">
-            <a
-              class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
-              href="#projects"
-            >
+            <a class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
+              href="#projects">
               Projects
             </a>
           </li>
           <li class="group relative">
-            <a
-              class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
-              href="#contact"
-            >
+            <a class="block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition"
+              href="#contact">
               Contact
             </a>
           </li>
         </ul>
       </nav>
-      <button
-        @click="toggleNav"
+      <button @click="toggleNav"
         class="block text-slate-400 hover:text-slate-900 dark:hover:text-slate-50 md:hidden trasition-all"
-        title="Open navigation menu"
-      >
+        title="Open navigation menu">
         <svg xmlns="http://www.w3.org/2000/svg" class="size-8 fill-white" viewBox="0 0 448 512">
           <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-          <path
-            fill="#ffffff"
-            d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
-          />
+          <path fill="#ffffff"
+            d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
         </svg>
       </button>
-      <nav
-        v-if="isNavOpen"
-        class="fixed bg-tertiary-light top-0 right-0 bottom-0 h-screen w-1/2 md:hidden transition-all"
-      >
+      <nav v-if="isNavOpen"
+        class="fixed bg-tertiary-light top-0 right-0 bottom-0 h-screen w-1/2 md:hidden transition-all">
         <ul class="flex flex-col space-y-2">
           <li class="text-right">
-            <button
-              @click="toggleNav"
-              class="px-6 py-8 text-slate-400 hover:text-slate-900 dark:hover:text-slate-50"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="size-7 fill-white"
-                viewBox="0 0 448 512"
-              >
+            <button @click="toggleNav" class="px-6 py-8 text-slate-400 hover:text-slate-900 dark:hover:text-slate-50">
+              <svg xmlns="http://www.w3.org/2000/svg" class="size-7 fill-white" viewBox="0 0 448 512">
                 <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                <path
-                  fill="#ffffff"
-                  d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
-                />
+                <path fill="#ffffff"
+                  d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
               </svg>
             </button>
           </li>
           <li class="group relative w-full overflow-x-visible text-right">
-            <a
-              class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
-              href="#home"
-            >
+            <a class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
+              href="#home">
               Home
             </a>
           </li>
           <li class="group relative w-full overflow-x-visible text-right">
-            <a
-              class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
-              href="#skills"
-            >
+            <a class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
+              href="#skills">
               Skills
             </a>
           </li>
           <li class="group relative w-full overflow-x-visible text-right">
-            <a
-              class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
-              href="#experience"
-            >
+            <a class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
+              href="#experience">
               Experiences
             </a>
           </li>
           <li class="group relative w-full overflow-x-visible text-right">
-            <a
-              class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
-              href="#projects"
-            >
+            <a class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
+              href="#projects">
               Projects
             </a>
           </li>
           <li class="group relative w-full overflow-x-visible text-right">
-            <a
-              class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
-              href="#contact"
-            >
+            <a class="mx-4 block whitespace-nowrap px-3 py-2 text-sm md:text-xl font-semibold text-orange-950 hover:text-white no-underline transition dark:hover:text-slate-50"
+              href="#contact">
               Contact
             </a>
           </li>
--- src/components/mini_components/skills_page.vue
+++ src/components/mini_components/skills_page.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="wrapper px-12">
     <div class="flex justify-center mb-12">
-      <h3
-        class="text-xl sm:text-3xl md:text-5xl font-bold underline decoration-wavy uppercase text-primary"
-      >
+      <h3 class="text-xl sm:text-3xl md:text-5xl font-bold underline decoration-wavy uppercase text-primary">
         Skills
       </h3>
     </div>
     <div class="flex justify-around items-center w-full flex-wrap gap-2 sm:gap-10">
       <div
-        class="size-[100px] bg-white sm:size-[150px] md:size-[200px] border-2 border-opacity-60 rounded-lg flex justify-around items-center p-2 sm:p-5 hover:scale-105 hover:shadow-normal hover:bg-[#E8762C]"
-        v-for="(item, index) in icons"
-        :key="index"
-        :style="`border-color: ${item.color}`"
-        :class="`hover:bg-[${item.color}]`"
-      >
-        <img
-          class="size-[150px] w-full object-contain object-center"
-          :src="item.url"
-          :alt="item.name + ' icon'"
-        />
+        class="relative size-[100px] bg-white sm:size-[150px] md:size-[200px] border-2 border-opacity-60 rounded-lg flex justify-around items-center p-2 sm:p-5 hover:scale-105 hover:shadow-normal flex"
+        v-for="(item, index) in icons" :key="index" :style="`border-color: ${item.color}`"
+        :class="`${item.hoverColor}`">
+        <img class="size-[150px] w-full object-contain object-center" :src="item.url" :alt="item.name + ' icon'" />
+        <!-- <div
+          class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-25 text-white text-lg font-bold opacity-0 hover:opacity-100 transition-opacity duration-300">
+          {{ item.name }}
+        </div> -->
       </div>
     </div>
   </div>
@@ -27,81 +22,94 @@
 <script setup>
 const icons = [
   {
-    name: 'html',
+    name: 'HTML',
     url: '/icons/html.svg',
     color: '#E44D26',
-    animation: false
+    hoverColor: 'hover:bg-[#ffece5]'
   },
   {
-    name: 'css',
+    name: 'CSS',
     url: '/icons/css.svg',
     color: '#1172B8',
-    animation: false
+    hoverColor: 'hover:bg-[#e9eef7]'
   },
   {
-    name: 'javascript',
+    name: 'Javascript',
     url: '/icons/javascript.svg',
-    color: '#FFCA28'
+    color: '#FFCA28',
+    hoverColor: 'hover:bg-[#fff9e9]'
   },
   {
-    name: 'bootstrap',
+    name: 'Bootstrap',
     url: '/icons/bootstrap.svg',
-    color: '#563D7C'
+    color: '#563D7C',
+    hoverColor: 'hover:bg-[#ebe8f0]'
   },
   {
-    name: 'java',
+    name: 'Java',
     url: '/icons/java.svg',
-    color: '#E76F00'
+    color: '#E76F00',
+    hoverColor: 'hover:bg-[#ffefe4]'
   },
   {
-    name: 'python',
+    name: 'Python',
     url: '/icons/python.svg',
-    color: '#2170B0'
+    color: '#2170B0',
+    hoverColor: 'hover:bg-[#e9eef6]'
   },
   {
-    name: 'go',
+    name: 'Go',
     url: '/icons/go.svg',
-    color: '#00ACD7'
+    color: '#00ACD7',
+    hoverColor: 'hover:bg-[#ebf6fb]'
   },
   {
-    name: 'mysql',
+    name: 'MySQL',
     url: '/icons/mysql.svg',
-    color: '#F8981D'
+    color: '#F8981D',
+    hoverColor: 'hover:bg-[#fff3e7]'
   },
   {
-    name: 'dbeaver',
+    name: 'DBeaver',
     url: '/icons/dbeaver.svg',
-    color: '#45352F'
+    color: '#45352F',
+    hoverColor: 'hover:bg-[#e9e6e5]'
   },
   {
-    name: 'git',
+    name: 'Git',
     url: '/icons/git.svg',
-    color: '#EE513B'
+    color: '#EE513B',
+    hoverColor: 'hover:bg-[#ffede7]'
   },
   {
-    name: 'r',
+    name: 'R',
     url: '/icons/r.svg',
-    color: '#2066B8'
+    color: '#2066B8',
+    hoverColor: 'hover:bg-[#e9edf7]'
   },
   {
-    name: 'msexcel',
+    name: 'Microsoft Excel',
     url: '/icons/msexcel.svg',
-    color: '#215037'
+    color: '#215037',
+    hoverColor: 'hover:bg-[#e5eae7]'
   },
   {
-    name: 'matplotlib',
+    name: 'Matplotlib',
     url: '/icons/matplotlib.svg',
-    color: '#E4925C'
+    color: '#E4925C',
+    hoverColor: 'hover:bg-[#fef3ec]'
   },
   {
     name: 'pandas',
     url: '/icons/pandas.svg',
-    color: '#E4925C'
+    color: '#130754',
+    hoverColor: 'hover:bg-[#e4e0eb]'
   },
   {
-    name: 'tableau',
+    name: 'Tableau Software',
     url: '/icons/tableau.svg',
-    color: '#E8762C'
+    color: '#E8762C',
+    hoverColor: 'hover:bg-[#fff0e7]'
   }
 ]
 </script>
