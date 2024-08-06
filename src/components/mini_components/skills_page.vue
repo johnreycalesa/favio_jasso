e247aa7edd25079d88764e0ebee95d8a03ea58dd
John <johannaraymendoza@gmail.com>
2024-08-06 12:15:30 +0800

    Updated the skills page.

--- src/components/mini_components/skills_page.vue
+++ src/components/mini_components/skills_page.vue
@@ -12,112 +12,150 @@
         :key="index"
         :style="`border-color: ${item.color}`"
         :class="`${item.hoverColor}`"
+        @mouseover="showSkillName(item.name)"
+        @mouseleave="hideSkillName"
       >
         <img
           class="size-[150px] w-full object-contain object-center"
           :src="item.url"
           :alt="item.name + ' icon'"
         />
-        <!-- <div
-          class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-25 text-white text-lg font-bold opacity-0 hover:opacity-100 transition-opacity duration-300">
+        <div
+          v-if="showDialog && currentSkill === item.name"
+          class="absolute inset-x-0 -top-16 flex items-center justify-center text-white text-2xl font-bold transition-opacity duration-300 rounded-xl shadow-normal p-2"
+          :class="`${item.skillsNameBackground}`"
+          id="skillName"
+        >
           {{ item.name }}
-        </div> -->
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
+import { ref } from 'vue'
+
+const showDialog = ref(false)
+const currentSkill = ref('')
+
+function showSkillName(name) {
+  currentSkill.value = name
+  showDialog.value = true
+}
+
+function hideSkillName() {
+  showDialog.value = false
+  currentSkill.value = ''
+}
+
 const icons = [
   {
     name: 'HTML',
     url: '/icons/html.svg',
     color: '#E44D26',
+    skillsNameBackground: 'bg-[#E44D26]',
     hoverColor: 'hover:bg-[#ffece5]'
   },
   {
     name: 'CSS',
     url: '/icons/css.svg',
     color: '#1172B8',
+    skillsNameBackground: 'bg-[#1172B8]',
     hoverColor: 'hover:bg-[#e9eef7]'
   },
   {
     name: 'Javascript',
     url: '/icons/javascript.svg',
     color: '#FFCA28',
+    skillsNameBackground: 'bg-[#FFCA28]',
     hoverColor: 'hover:bg-[#fff9e9]'
   },
   {
     name: 'Bootstrap',
     url: '/icons/bootstrap.svg',
     color: '#563D7C',
+    skillsNameBackground: 'bg-[#563D7C]',
     hoverColor: 'hover:bg-[#ebe8f0]'
   },
   {
     name: 'Java',
     url: '/icons/java.svg',
     color: '#E76F00',
+    skillsNameBackground: 'bg-[#E76F00]',
     hoverColor: 'hover:bg-[#ffefe4]'
   },
   {
     name: 'Python',
     url: '/icons/python.svg',
     color: '#2170B0',
+    skillsNameBackground: 'bg-[#2170B0]',
     hoverColor: 'hover:bg-[#e9eef6]'
   },
   {
     name: 'Go',
     url: '/icons/go.svg',
     color: '#00ACD7',
+    skillsNameBackground: 'bg-[#00ACD7]',
     hoverColor: 'hover:bg-[#ebf6fb]'
   },
   {
     name: 'MySQL',
     url: '/icons/mysql.svg',
     color: '#F8981D',
+    skillsNameBackground: 'bg-[#F8981D]',
     hoverColor: 'hover:bg-[#fff3e7]'
   },
   {
     name: 'DBeaver',
     url: '/icons/dbeaver.svg',
     color: '#45352F',
+    skillsNameBackground: 'bg-[#45352F]',
     hoverColor: 'hover:bg-[#e9e6e5]'
   },
   {
     name: 'Git',
     url: '/icons/git.svg',
     color: '#EE513B',
+    skillsNameBackground: 'bg-[#EE513B]',
     hoverColor: 'hover:bg-[#ffede7]'
   },
   {
     name: 'R',
     url: '/icons/r.svg',
     color: '#2066B8',
+    skillsNameBackground: 'bg-[#2066B8]',
     hoverColor: 'hover:bg-[#e9edf7]'
   },
   {
     name: 'Microsoft Excel',
     url: '/icons/msexcel.svg',
     color: '#215037',
+    skillsNameBackground: 'bg-[#215037]',
     hoverColor: 'hover:bg-[#e5eae7]'
   },
   {
     name: 'Matplotlib',
     url: '/icons/matplotlib.svg',
     color: '#E4925C',
+    skillsNameBackground: 'bg-[#E4925C]',
     hoverColor: 'hover:bg-[#fef3ec]'
   },
   {
     name: 'pandas',
     url: '/icons/pandas.svg',
     color: '#130754',
+    skillsNameBackground: 'bg-[#130754]',
     hoverColor: 'hover:bg-[#e4e0eb]'
   },
   {
     name: 'Tableau Software',
     url: '/icons/tableau.svg',
     color: '#E8762C',
+    skillsNameBackground: 'bg-[#E8762C]',
     hoverColor: 'hover:bg-[#fff0e7]'
   }
 ]
 </script>
+
 <style scoped></style>
